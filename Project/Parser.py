e5c3bf728b6458c28253c10e0c770d3ca650b1f7
SKOLX <d.butakov2005@gmail.com>
2024-10-23 22:33:25 +0500

    добавил парсер для постов хабра + потыкался в main

---
+++ Project/Parser.py
@@ -0,0 +1,30 @@
+﻿from bs4 import BeautifulSoup
+import requests
+
+
+class ParserHabr:
+    def __init__(self):
+        self.habr_link = "https://habr.com/ru/posts/"
+
+    def get_habr_posts(self):
+        posts = []
+        try:
+            html = requests.get(self.habr_link)
+            html.raise_for_status()  # Проверка кода ответа (200 - OK)
+            soup = BeautifulSoup(html.content, 'html.parser')
+            post_elements = soup.find_all('div', class_='tm-post-snippet__content')
+
+            for post_element in post_elements:
+                post_text = ""
+                paragraphs = post_element.find_all('p')
+                for paragraph in paragraphs:
+                    post_text += (paragraph.text.strip() + "\n")
+                post_text += "\nСсылка, откуда был взят пост : https://habr.com/ru/posts/"
+                posts.append(post_text)  # Добавляем текст поста в массив
+
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при запросе к Habr: {e}")
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")
+
+        return posts  # Возвращаем массив с постами
--- Project/main.py
+++ Project/main.py
@@ -2,22 +2,26 @@
 
 import requests
 
+import Parser
+
 
 class Script:
     def __init__(self):
         self.key = "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"
         self.model = {"perplexity": "perplexity/llama-3.1-sonar-huge-128k-online",
-                      "gpt": "openai/o1-mini-2024-09-12"}
+                      "gpt": "openai/o1-mini-2024-09-12",
+                      "free": "meta-llama/llama-3.1-405b-instruct:free"}
+        self.parserHabr = Parser.ParserHabr()
 
     def get_chat_response(self, message, model):
         """Отправляет сообщение в OpenRouter и возвращает ответ.
             Args:
               message (str): Сообщение для отправки.
-              model: модель ИИ
+              model: модель ИИ (брать из словарика моделей)
             Returns:
               str: Ответ от OpenRouter.
             """
-    
+
         response = requests.post(
             url="https://openrouter.ai/api/v1/chat/completions",
             headers={
@@ -33,12 +37,26 @@ class Script:
                 ]
             })
         )
-    
+
         if response.status_code == 200:
             data = response.json()
             reply = data
-            return reply
+            return self.get_chat_message(reply)
         else:
             print(f"Ошибка: {response.status_code}")
             print(response.text)
             return None
+    
+    def get_chat_message(self, message):
+        return message["choices"][0]["message"]["content"]
+
+if __name__ == "__main__":
+    foo = Script()
+    posts = foo.parserHabr.get_habr_posts()
+    post = posts[1]
+    message = ("Напиши sammary для этого поста. И сделай так, чтобы это можно было выложить в тг канал. "
+               "Красиво оформи смайликами и тд тп"
+               "А В конце просто, что информация была взята отсюда: "
+               f"Текст: {post}")
+    print(message)
+    print(foo.get_chat_response(message, foo.model["free"]))
