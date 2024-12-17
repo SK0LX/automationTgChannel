54415ed46254c03638e2da4e32b2709182e6f40e
SKOLX <d.butakov2005@gmail.com>
2024-12-17 18:21:50 +0500

    core 2.0

---
+++ Project/IAiClient.py
@@ -0,0 +1,28 @@
+﻿import json
+
+import requests
+
+
+class IAiClient:
+    def send_message(self, model: str, message: str) -> str:
+        raise NotImplementedError
+
+
+class OpenRouterClient(IAiClient):
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    def send_message(self, model: str, message: str) -> str:
+        response = requests.post(
+            url="https://openrouter.ai/api/v1/chat/completions",
+            headers={"Authorization": f"Bearer {self.api_key}"},
+            data=json.dumps({
+                "model": model,
+                "messages": [{"role": "user", "content": message}]
+            }),
+        )
+        if response.status_code == 200:
+            data = response.json()
+            return data["choices"][0]["message"]["content"]
+        else:
+            raise Exception(f"Ошибка API: {response.status_code}, {response.text}")
